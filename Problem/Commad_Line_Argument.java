db96e4c03eb5047bead59dc917d33550cc605ead
Shubham2928 <152942576+Shubham2928@users.noreply.github.com>
2024-08-30 12:02:20 +0530

    Add files via upload

---
+++ Problem/Commad_Line_Argument.java
@@ -0,0 +1,12 @@
+// Command line argument in java
+
+class Argument
+{
+    public static void main(String Args[])
+    {
+        for(String t: Args)
+        {
+            System.out.println(t);
+        }
+    }
+}
---
+++ Problem/HelloWorld.java
@@ -0,0 +1,9 @@
+// Question - Print Hello world..
+
+class HelloWorld
+{
+    public static void main(String args[])
+    {
+        System.out.println("Hello World...");
+    }
+}
