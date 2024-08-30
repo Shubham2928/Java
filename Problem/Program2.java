29f1c3040e5255f376c3fd969e95b48f58efdd5a
Shubham2928 <152942576+Shubham2928@users.noreply.github.com>
2024-08-30 11:50:52 +0530

    Add files via upload

---
+++ Problem/Program2.java
@@ -0,0 +1,15 @@
+// print integer in java
+
+class Integer
+{
+    public static void main(String args[])
+    {
+        int c;   // declaring variable
+
+        // using for loop to repeat instruction
+        for(c = 1; c <= 10; c++)
+        {
+            System.out.println(c);
+        }
+    }
+}
